3abcdebdf4b6e15d3b2b67b16a641dfabfc47e4e
Xoaquin Sanchez Varsallona <xsanchezvarsallona@mail.austral.edu.ar>
2024-10-29 16:57:23 -0300

    Added progressBar to MedComp.

--- src/components/medication/medComp/MedComp.tsx
+++ src/components/medication/medComp/MedComp.tsx
@@ -12,11 +12,35 @@ type MedInfoComp = {
 const MedComp = ({name, dosis, time, days, pillsLeft} : MedInfoComp) => {
     return (
       <div className="p-3" >
-        <div className="p-3" >
-          <MedInfo time={time} days={days} pillsLeft={pillsLeft}/>
-        </div>      
+          {
+          //Hacer que aca ponga la imagen del vector
+          }
+          <div>
+            <h1 className="text-title font-bold text-scale-1000">{name}</h1>
+            <MedInfo time={time} days={days} pillsLeft={pillsLeft}/>  
+          </div>  
+          <ProgressBar pills={pillsLeft} dosis={dosis} />  
       </div>
     )
 }
 
 export default MedComp;
+
+type ProgressBarProps = {
+  pills: number,
+  dosis: number
+}
+
+const ProgressBar = ({pills, dosis} : ProgressBarProps) => {
+  return (
+    <div className="w-full bg-scale-700 rounded-full h-2">
+      <div
+        className="bg-scale-1000 h-2 rounded-full transition-all duration-300"
+        style={{ width: `${(pills / dosis) * 100}%` }}
+        role="progressbar"
+        aria-valuemin={20}
+        aria-valuemax={100}
+      ></div>
+    </div>
+  );
+};
