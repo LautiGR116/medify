0d9db7aec0c99fbe038487300599783dd3439592
Xoaquin Sanchez Varsallona <xsanchezvarsallona@mail.austral.edu.ar>
2024-11-05 19:23:18 -0300

    NavBarButton

--- src/components/navbar/Button.tsx
+++ src/components/navbar/Button.tsx
@@ -2,34 +2,38 @@ import React from "react";
 import { ButtonHTMLAttributes } from "react";
 
 const ButtonVariants = {
-  active : {
-    buttonVariant : "size-10 bg-primary-500 flex items-center justify-center rounded-full",
-    iconVariant : "text-scale-1000 size-6"
+  active: {
+    buttonVariant: "bg-primary-500",
+    iconVariant: "text-scale-1000 size-6"
   },
-  disable : {
-    buttonVariant : "size-10 bg-scale-100 flex items-center justify-center rounded-full",
-    iconVariant : "size-6"
+  disable: {
+    buttonVariant: "bg-scale-100",
+    iconVariant: "text-scale-600 size-6"
   }
-}
+};
+
 interface ButtonI extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant: "active" | "disable";
-  icon : JSX.Element;
+  icon: JSX.Element;
 }
 
-const NavBarButton = ({variant, icon, ...props} : ButtonI) => {
+const NavBarButton = ({ variant, icon, ...props }: ButtonI) => {
   const buttonVariant = ButtonVariants[variant].buttonVariant;
   const iconVariant = ButtonVariants[variant].iconVariant;
-  console.log(iconVariant);
-  console.log(buttonVariant);
-  console.log(icon);
-  return (
+  const [active, setActive] = React.useState(false);
+  
 
-    <button className="rounded-full size-10 flex" {...props} >
-      <div className={buttonVariant}>
-        {React.cloneElement(icon, {className: iconVariant})}
+  return (
+    <button
+      className={`size-10 flex items-center justify-center rounded-full ${buttonVariant}`}
+      {...props}
+    >
+      {/* Div que ocupa todo el tamaño del botón y centra el icono */}
+      <div className="flex items-center justify-center w-full h-full">
+        {React.cloneElement(icon, { className: `${iconVariant} flex-shrink-0` })}
       </div>
     </button>
-  ) 
-}
+  );
+};
 
 export default NavBarButton;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,26 +1,5 @@
 import React, { ButtonHTMLAttributes, useEffect, useState } from "react"
-import { MynauiCalendar } from "../svg/navbar/Calendar"
-import { MdiLightHeart } from "../svg/navbar/Heart"
-import { IcRoundHome } from "../svg/navbar/Home"
-import { MaterialSymbolsLightPill } from "../svg/navbar/Pill"
-import { Button } from "../../stories/Button"
-
-const ButtonVariants = {
-  active : {
-    buttonVariant : "size-10 bg-scale-1000 flex items-center justify-center rounded-full",
-    iconVariant : {
-      color: "text-primary-100",
-      props: "size-6"
-    }
-  },
-  disable : {
-    buttonVariant : "size-10 bg-scale-100 flex items-center justify-center rounded-full",
-    iconVariant : {
-      color: "text-primary-300",
-      props: "size-6"
-    }
-  }
-}
+import NavBarButton from "./Button"
 
   interface ButtonI extends ButtonHTMLAttributes<HTMLButtonElement> {
     variant: "active" | "disable";
@@ -32,27 +11,15 @@ const ButtonVariants = {
     args: ButtonI;
   }
 
-const NavBar = () => {
+const NavBar = (pages  : Button[]) => {
   const [page , setPage] = useState("")
-
-  function isActive(button: string) {
-    return page === button ? ButtonVariants.active : ButtonVariants.disable
-  }
 
   return (
       <div className="p-3 w-navbar h-navbar bg-scale-100 flex justify-between">
-        <button className={isActive("home").buttonVariant} style={{ fill: 'black' }} onClick={() => setPage("home")}>
-          <IcRoundHome className={isActive("home").iconVariant.props} color={isActive("medication").iconVariant.color}/>
-        </button>
-        <button className={isActive("medication").buttonVariant} onClick={() => setPage("medication")}>
-          <MaterialSymbolsLightPill className={isActive("medication").iconVariant.props} color={isActive("medication").iconVariant.color}/>
-        </button>
-        <button className={isActive("appointment").buttonVariant} onClick={() => setPage("appointment")}>
-          <MynauiCalendar className={isActive("appointment").iconVariant.props} color={isActive("medication").iconVariant.color}/>
-        </button>
-        <button className={isActive("wellfare").buttonVariant} onClick={() => setPage("wellfare")}>
-          <MdiLightHeart className={isActive("wellfare").iconVariant.props} color={isActive("medication").iconVariant.color}/>
-        </button>
+        {pages.map((page) => {
+          return (
+            <NavBarButton {...page.args} onClick={() => setPage(page.args.title)} />
+        }}
       </div>
     );
 }
