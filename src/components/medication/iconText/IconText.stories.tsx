cf87051ee66a33a250e32b9571fb0a6b1bfac110
Marcos <mcarrizoaslanian@mail.austral.edu.ar>
2024-11-09 18:49:01 -0300

    fixes and improvements

--- src/components/medication/iconText/IconText.stories.tsx
+++ src/components/medication/iconText/IconText.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import IconText from './IconText';
-import Pill from "../../svg/pill.png";
+import { HalfFilledPill } from "../../svg/medication/Medication";
 
 export default {
   title: 'IconText',
@@ -9,7 +9,8 @@ export default {
 
 export const Default = {
   args : {
-    icon: Pill,
-    text: "X amount of pills left"
+    icon: <HalfFilledPill width={16} height={16} />,
+    text: "X amount of pills left",
+    size: "16",
   }
 }
---
+++ src/components/svg/common/Common.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import type { SVGProps } from 'react';
+
+export function RoundedArrow(props: SVGProps<SVGSVGElement>) {
+    return <svg className={props.className} xmlns="http://www.w3.org/2000/svg" width={props.width} height={props.height} viewBox="0 0 1024 1024"><path fill="currentColor" d="M104.704 685.248a64 64 0 0 0 90.496 0l316.8-316.8l316.8 316.8a64 64 0 0 0 90.496-90.496L557.248 232.704a64 64 0 0 0-90.496 0L104.704 594.752a64 64 0 0 0 0 90.496"/></svg>
+}
