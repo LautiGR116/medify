4c8e693fdf324744b4d62163c01bcfade83b0ed0
Máximo Sallemi <msallemi@mail.austral.edu.ar>
2024-11-11 19:28:22 -0300

    iconos de la navbar

--- src/components/common/popup/Popup.tsx
+++ src/components/common/popup/Popup.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from "../Button.tsx";
+import Button from "../Button";
 
 interface PopupProps {
     title: string;
@@ -12,14 +12,15 @@ const Popup: React.FC<PopupProps> = ({ title, text, destructiveButtonText, confi
     const buttonWidth = 90
     return (
         <div className="w-[224px] h-[180px] flex flex-col items-center justify-between bg-scale-100 rounded-[10px] p-2">
+            {/* div del título */}
             <div className="w-full text-scale-1000 text-title2 font-semibold text-center">
                 {title}
             </div>
+            {/* div del texto */}
             <div className="py-2 px-4 text-scale-1000 text-caption1 font-regular text-center">
                 {text}
             </div>
-
-            {/* Button container with flex-row, spaced to the left and right */}
+            {/* div de los buttons */}
             <div className="mt-2 flex w-full justify-between">
                 <Button
                     variant="filled"
