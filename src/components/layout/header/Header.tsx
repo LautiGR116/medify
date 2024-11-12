209dd57be1cf871c686a61ef79e1b801a708b16b
BrunoIacovone <iacovonebru@gmail.com>
2024-11-12 15:12:42 -0300

    no toquen mas el header

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,6 +1,6 @@
 import { ReactElement } from "react";
 import React from "react";
-import { RoundedArrow } from "../../svg/common/Common";
+import {RoundedArrow} from "../../svg/common/Common";
 
 const HeaderVariants = {
     title: {
@@ -20,34 +20,30 @@ interface Icon {
     icon: ReactElement;
 }
 
-const Header = ({ name, title, comp }: HeaderI) => {
+const Header = ({name, title, comp }: HeaderI) => {
     function getTitleVariant() {
         return name === "home" ? HeaderVariants.title.home.join(" ") : HeaderVariants.title.default.join(" ");
     }
 
     return (
-        <div className="flex w-header h-header rounded-t-[53px] bg-scale-100 items-end px-4 py-2 justify-center">
-            {name !== "home" ? (
-                <>
-                    <div className="flex-shrink-0 mb-2">
-                        <RoundedArrow className={`${HeaderVariants.icon.join(" ")} transform -rotate-90`} />
-                    </div>
-                    <span className={`${getTitleVariant()} flex-grow text-center mb-2`}>
+        <div className="flex w-header h-header rounded-t-[53px] bg-scale-100 items-end px-4 py-2 justify-between">
+            { name !== "home" ? (
+                    <>
+                        <div className="flex-shrink-0 mb-2">
+                            <RoundedArrow className={`${HeaderVariants.icon.join(" ")} transform -rotate-90`} />
+                        </div>
+                        <span className={`${getTitleVariant()} flex-1 text-center mb-2`}>
             {title}
           </span>
-                    {comp ? (
-                        <div className="flex-shrink-0 mb-2">
-                            {React.cloneElement(comp.icon, { className: HeaderVariants.icon.join(" ") })}
-                        </div>
-                    ) : (
-                        <div className="w-8 mb-2" />
-                        )}
-                </>
-            ) : (
+                    </>) :
                 <span className={`${getTitleVariant()} flex-1 mb-2 text-left`}>
-          {title}
-        </span>
-            )}
+           {title}
+          </span>
+            }
+            <div className="flex-shrink-0 mb-2">
+                {comp ? React.cloneElement(comp.icon, {className: HeaderVariants.icon.join(" ")}) :
+                    <div className="w-8 h-8"></div>}
+            </div>
         </div>
     );
 };
